657efb1531001c3855558ea2bc4d96c852fcf167
Angharad Caswell <ange.caswell1@gmail.com>
2021-10-04 11:08:23 +0000

    add instructions for when the players have given their names

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("hello!")
+
+def players_name():
+    player_one = input("Player one, what is your name?\n")
+    player_two = input("Player two, what is your name?\n")
+    welcome_message = print("Welcome " + player_one + " and " + player_two + ", lets play Connect Four!\n How to play:\n Take it in turns to add a piece to a column. When you have 4 pieces next to eachtoher you win! \n The pieces can be vertical, horizontal or diagonal. Good luck!" )
+    return welcome_message
+
+players_name()
+
