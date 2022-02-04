ff909283f5670a0de72d12ce22d4ea55c06c3712
Angharad Caswell <ange.caswell1@gmail.com>
2022-02-04 17:38:09 +0000

    changes made from validating code

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from termcolor import colored, cprint
 ROW_NUM = 6
 COL_NUM = 7
 
+
 def open_message():
     cprint("#######################################", 'blue')
     print("#                                     #")
@@ -21,6 +22,7 @@ def open_message():
     print("#                                     #")
     cprint("#######################################", 'blue')
 
+
 def players_name():
     """
     Function to get players names and print instructions
@@ -31,9 +33,8 @@ def players_name():
     global player_two
     player_two = input("Player two, what is your name?\n")
     print("-" * 40)
-    welcome_message = print(f"Welcome {player_one} and {player_two}, lets play Connect Four!\n\n How to play:\n\n ##Take it in turns to add a piece to a column. \n When you have 4 pieces next to eachtoher you win! \n\n ##The pieces can be vertical, horizontal or diagonal. \n\n Good luck! \n\n\n\n" )
+    welcome_message = print(f"Welcome {player_one} and {player_two}, lets play Connect Four!\n\n How to play:\n\n ##Take it in turns to add a piece to a column. \n When you have 4 pieces next to eachtoher you win! \n\n ##The pieces can be vertical, horizontal or diagonal. \n\n Good luck! \n\n\n\n")
     return welcome_message
-
 
 
 def create_board():
@@ -45,7 +46,6 @@ def create_board():
     return board
 
 
-
 def start_game():
     open_message()
     players_name()
@@ -54,25 +54,27 @@ def start_game():
 
 start_game()
 
+
 def validate_data(value):
         """ Validate data by checking for interger and number <= 6"""
         try:
             int(value)
             if int(value) > 6:
-                raise ValueError(f"You can only choose a column from 0 and upto 6")
-            
+                raise ValueError(f"You can only choose a column from 0 and upto 6")            
         except ValueError as e:
             cprint(f"Invalid data: {e}, please try again.\n", 'red')
             return False
 
         return True
 
+
 def next_row(board, col): 
         """ looking for the next available row on the column that the user selected. An available row will have a value fo 0 which will be changed to the users number"""       
         for row in range(ROW_NUM):
             if board[row][col] == 0:
-                return row
-        
+                return row               
+
+
 def winning_move(board, piece):
         """ Credit for code: Keith Galli - Youtube """
         """ Check horizontal locations for win"""
@@ -109,13 +111,13 @@ while not game_over:
     Link to new row function to find the next available row for the user to place a piece. 
     """
     while True:
-            if turn==0:  
+            if turn == 0:  
                 user_choice = input(f"{player_one.capitalize()}, choose a column to drop a piece (0-6):\n")                
                 if validate_data(user_choice):
                     col = int(user_choice)
-                    if board[5][col]==0:
+                    if board[5][col] == 0:
                         row = next_row(board, col)
-                        board[row][col]=1
+                        board[row][col] = 1
                         print(np.flip(board, 0)) 
                         break                         
                     else:
@@ -124,13 +126,12 @@ while not game_over:
                 if winning_move(board, 1):
                     cprint(f"{player_one.capitalize()} wins!!!!", "green")
                     cprint(f"{player_two.capitalize()} unlucky!!!!", "red")
-                    game_over=True
-                            
+                    game_over = True                           
             else:
                 user_choice = input(f"{player_two.capitalize()} choose a column to drop a piece (0-6):\n")                
                 if validate_data(user_choice):
                     col = int(user_choice)
-                    if board[ROW_NUM-1][col]==0:
+                    if board[ROW_NUM-1][col] == 0:
                         row = next_row(board, col)
                         board[row][col] = 2
                         print(np.flip(board, 0)) 
@@ -141,19 +142,17 @@ while not game_over:
                 if winning_move(board, 2):
                     cprint(f"{player_two.capitalize()} wins!!!!", "green")
                     cprint(f"Unlucky, {player_one.capitalize()}!!!", "red")
-                    game_over=True
-                                            
-    turn+=1
-    turn=turn % 2
+                    game_over = True                                          
+    turn += 1
+    turn = turn % 2
     if game_over:
         cprint("*************************", 'yellow')
         cprint("*************************", 'yellow')
         cprint("   G A M E    O V E R", 'yellow')
         cprint("*************************", 'yellow')
         cprint("*************************", 'yellow')
-        continue_game=input("Would you like to play again? Y/N \n")
-        if continue_game== "Y":
+        continue_game = input("Would you like to play again? Y/N \n")
+        if continue_game == "Y":
             start_game()
         else:
-            print("Thank you for playing!")
-        
+            print("Thank you for playing!")   
